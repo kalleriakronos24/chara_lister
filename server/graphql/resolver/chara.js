63248319dfb12295da8c12e126cb186a23d1274d
Kalleria <kalleriakronos24@gmail.com>
2019-11-21 22:31:46 +0800

    Added Elements, Skills, Race, Passives into the Characters

--- server/graphql/resolver/chara.js
+++ server/graphql/resolver/chara.js
@@ -1,86 +1,98 @@
 const Hero = require("../../models/hero");
 const Creator = require("../../models/user");
 const path = require("path");
-const multer = require('multer');
-const express = require('express');
-const router = express.Router()
-const uuidv4 = require('uuid/v4');
+const multer = require("multer");
+const express = require("express");
+const router = express.Router();
+const uuidv4 = require("uuid/v4");
 
-      const DIR = './public/'
+const DIR = "./public/";
 
-      const storage = multer.diskStorage({
-          destination : (req, file, cb) => {
-              cb(null, DIR);
-          },
-          filename : (req, file, cb) => {
-              const filename = file.originalname.toLocaleLowerCase().split(' ').join('-');
-              cb(null, uuidv4() + '-' + filename)
-          }
-      })
-
-      var upload = multer({
-        storage : storage,
-        fileFilter : (req, file, cb) => {
-            if(file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg'){
-                cb(null, true)
-            }else{
-                cb(null, false);
-                return cb(new Error('Only .png, .jpg, .jpeg format allowed'))
-            }
-        }
-      })
-
-router.post('/add-new-hero', upload.fields([{ name : 'thumbnail', maxCount : 1}, { name : 'sprite', maxCount: 4}]), async (req, res, next) => {
-
-    try{
-
-    console.table(req.body);
-    console.log(req.files);
-    console.log(req.files['thumbnail'][0].filename);
-    
-    
-
-    const sprites = [];
-    const url = req.protocol + '://'+ req.get('host');
-    for(var i = 0; i < req.files['sprite'].length; i++){
-        sprites.push(url + '/public/' + req.files['sprite'][i].filename)
-    }
-
-    const thumb = req.protocol + '://' + req.get('host') + '/public/' + req.files['thumbnail'][0].filename
-
-  const hero = new Hero(
-    {
-      name: req.body.hero_name,
-      hp: req.body.hp,
-      mana: req.body.mana,
-      passives: req.body.passives,
-      about : req.body.about,
-      alive: true,
-      thumbnail : thumb,
-      sprite : sprites,
-      race: req.body.race,
-      creator: "5dbc4237760587191851a3d9"
-    }
-  ); 
-    
-  for(var i in req.body.skills){
-    hero.skills.push(JSON.parse(req.body.skills[i]));
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, DIR);
+  },
+  filename: (req, file, cb) => {
+    const filename = file.originalname
+      .toLocaleLowerCase()
+      .split(" ")
+      .join("-");
+    cb(null, uuidv4() + "-" + filename);
   }
+});
 
-  await hero.save()
-
-  res.status(200).send("OK");
-
-  const creator = await Creator.findById("5dbc4237760587191851a3d9");
-  if (!creator) {
-    throw new Error("Creator doesnt exist");
+var upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error("Only .png, .jpg, .jpeg format allowed"));
+    }
   }
-  await creator.createdHero.push(hero);
-  await creator.save();
-}catch(err){
-    console.log(err);
-}
-})
+});
+
+router.post(
+  "/add-new-hero",
+  upload.fields([
+    { name: "thumbnail", maxCount: 1 },
+    { name: "sprite", maxCount: 4 }
+  ]),
+  async (req, res, next) => {
+    try {
+      console.table(req.body);
+      console.log(req.files);
+      console.log(req.files["thumbnail"][0].filename);
+
+      const sprites = [];
+      const url = req.protocol + "://" + req.get("host");
+      for (var i = 0; i < req.files["sprite"].length; i++) {
+        sprites.push(url + "/public/" + req.files["sprite"][i].filename);
+      }
+
+      const thumb =
+        req.protocol +
+        "://" +
+        req.get("host") +
+        "/public/" +
+        req.files["thumbnail"][0].filename;
+
+      const hero = new Hero({
+        name: req.body.hero_name,
+        hp: req.body.hp,
+        mana: req.body.mana,
+        passives: req.body.passives,
+        about: req.body.about,
+        alive: true,
+        thumbnail: thumb,
+        sprite: sprites,
+        race: req.body.race,
+        creator: "5dbc4237760587191851a3d9"
+      });
+
+      for (var i in req.body.skills) {
+        hero.skills.push(JSON.parse(req.body.skills[i]));
+      }
+
+      await hero.save();
+
+      res.status(200).send("OK");
+
+      const creator = await Creator.findById("5dbc4237760587191851a3d9");
+      if (!creator) {
+        throw new Error("Creator doesnt exist");
+      }
+      await creator.createdHero.push(hero);
+      await creator.save();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
 
 module.exports = router;
-     
